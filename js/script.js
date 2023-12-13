253d46ef188e49c5b151ab1a3c307042936fda84
TobiaRigon <rigontobia@gmail.com>
2023-12-13 15:59:06 +0100

    Feat:milestone 1 + bonus

--- js/script.js
+++ js/script.js
@@ -6,12 +6,33 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-     
+      currentMail:"",
+     mailList:[],
+
     }
   },
 
   methods: {
+    newMail() {
+      axios.get( "https://flynn.boolean.careers/exercises/api/random/mail" )
+      .then((response)=> {
+       this.currentMail = response.data.response;
    
+       this.mailList.push(this.currentMail);
+   
+       console.log(this.mailList);
+   
+      })
+    }
+  
+},
+
+mounted() {
+
+  for (let i = 0; i < 10; i++) {
+    this.newMail();
+  }
+  
 },
 
   
